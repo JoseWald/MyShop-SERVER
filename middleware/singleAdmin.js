068935ce65196dc7b72de84a561bfa2e8f95b874
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-08-05 22:02:41 +0300

    creation d'un middleware pour assurer qu'il n'y aura qu'un seul admin

---
+++ middleware/singleAdmin.js
@@ -0,0 +1,18 @@
+const Amdin=require('../model/adminModel');
+
+const singleAdmin= async (req,res,next)=>{
+    try{
+        const countAdmin=Amdin.countDocuments()
+        if(countAdmin>=1){
+            return res.status(400).json({message:'il ne doit y avoir qu\'un seul admin'});
+        }
+        next();
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message:'erreur interne au serveur'});
+    }
+
+}
+
+module.exports=singleAdmin;
