2b878b5a36054e71a605bf01d73d635f4d1755a3
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-08-06 14:27:29 +0300

    mise à jour de authMiddleware pour afficher le token sur react

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,25 +2,19 @@ const jwt=require('jsonwebtoken')
 const admin=require('../model/adminModel');
 
 const security=async (req,res,next)=>{
-    let token;
-
-    if(req.header.authorization && req.header.authorization.startWith('Bearer')){
-        try{
-            token=req.header.authorization.split(' ')[1];
-            const decoded=jwt.verify(token,process.env.JWT_SECRET);
-
-            req.user=await admin.findById(decoded.id).select('-password');
-
-            next();
-
-        }catch(error){
-            console.error(error);
-            res.status(401).json({message:'token non obtenu'})
-        }
-    }
+    let token=req.header('Authorization').replace('Bearer','');
 
     if(!token)
         res.status(401).json({message:'token fail , pas autorisé'});
+
+    try{
+        const decoded=jwt.decode(token,process.env.JWT);
+        req.user=decoded.user;
+        next();
+
+    }catch(err){
+        res.status(401).json({message:'token invalid',error:err})
+    }
 
 }
 
