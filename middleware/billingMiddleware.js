b45d04fbacacdce11a00258e10f1f102d9ea3458
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-08-12 11:12:29 +0300

    mise à jour de billprotection Middleware

--- middleware/billingMiddleware.js
+++ middleware/billingMiddleware.js
@@ -1,12 +1,16 @@
-exports.billingProtection=(err,req,res,next)=>{
-    const {customer,givenAmount}=req.body;
-    const regex='/\d/'
-    if(customer.trim()==='' || regex.test(customer) ){
-        return res.status(400).json({message:"Invalid Name"});
-    }
-    if(isNaN(givenAmount) && givenAmount <=0){
-        return res.status(400).json({message:"The customer's money is invalid"});
+const billingProtection = (req, res, next) => {
+    const { customer, givenAmount } = req.body;
 
+    const regex = /\d/;
+    if (customer.trim() === '' || regex.test(customer)) {
+        return res.status(400).json({ message: "Invalid Name" });
     }
+
+    if (isNaN(givenAmount) || givenAmount <= 0) {
+        return res.status(400).json({ message: "The customer's money is invalid" });
+    }
+
     next();
-}
+};
+
+module.exports={billingProtection};
